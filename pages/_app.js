c30788ab2bb61eef81a42de6a85d36e915f2af5c
JuanCruzEusebi <juancruzeusebi@gmail.com>
2023-04-06 14:18:36 -0300

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
-import '@/appstyles/globals.css'
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
