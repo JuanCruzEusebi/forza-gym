8ca869c11d0ffaff6c4a1b80324edd65b48f5bc5
JuanCruzEusebi <juancruzeusebi@gmail.com>
2023-04-09 13:25:35 -0300

    working on card items

--- components/ExperienciaForza/ExperienciaForza.jsx
+++ components/ExperienciaForza/ExperienciaForza.jsx
@@ -1,33 +1,110 @@
 import style from "./expforza.module.scss";
+import Image from "next/image";
 
 export default function Test() {
   return (
     <div className={style.mainContainer}>
-      <div className={style.heading}>Heading</div>
-      <div className={style.images} style={{ flex: "1" }}>
+      <div className={style.headingContainer}>
+        <h1>
+          ¿Que es la <br /> <span>#Experiencia</span>FORZA?
+        </h1>
+      </div>
+      <div className={style.cardContainer} style={{ flex: "1" }}>
         <div
           style={{ width: "300px", height: "400px" }}
           className={style.single}
         >
-          Prueba Uno
+          <Image
+            src={"/imgs/Hibrido.png"}
+            width={280}
+            height={392}
+            alt="card"
+            className={style.img}
+          />
+          <p>
+            El equipo de nuestro centro médico trabaja de forma coordinada con
+            los profesores de nuestro gimnasio.
+          </p>
         </div>
         <div
           style={{ width: "300px", height: "400px" }}
           className={style.single}
         >
-          Prueba Uno
+          <Image
+            src={"/imgs/Hibrido.png"}
+            width={280}
+            height={392}
+            alt="card"
+            className={style.img}
+          />
+          <p>
+            Nuestros planes incluyen una valoración kinesiológica gratis al mes.
+            Ideal para corregir patologías posturales o rehabilitaciones.
+          </p>
         </div>
         <div
           style={{ width: "300px", height: "400px" }}
           className={style.single}
         >
-          Prueba Uno
+          <Image
+            src={"/imgs/Hibrido.png"}
+            width={280}
+            height={392}
+            alt="card"
+            className={style.img}
+          />
+          <p>
+            Buscamos lo mejor de cada disciplina y lo adaptamos a tus
+            necesidades y objetivos.
+          </p>
         </div>
         <div
           style={{ width: "300px", height: "400px" }}
           className={style.single}
         >
-          Prueba Uno
+          <Image
+            src={"/imgs/Hibrido.png"}
+            width={280}
+            height={392}
+            alt="card"
+            className={style.img}
+          />
+          <p>
+            El equipo de nuestro centro médico trabaja de forma coordinada con
+            los profesores de nuestro gimnasio.
+          </p>
+        </div>
+        <div
+          style={{ width: "300px", height: "400px" }}
+          className={style.single}
+        >
+          <Image
+            src={"/imgs/Hibrido.png"}
+            width={280}
+            height={392}
+            alt="card"
+            className={style.img}
+          />
+          <p>
+            Nuestros planes incluyen una valoración kinesiológica gratis al mes.
+            Ideal para corregir patologías posturales o rehabilitaciones.
+          </p>
+        </div>
+        <div
+          style={{ width: "300px", height: "400px" }}
+          className={style.single}
+        >
+          <Image
+            src={"/imgs/Hibrido.png"}
+            width={280}
+            height={392}
+            alt="card"
+            className={style.img}
+          />
+          <p>
+            Buscamos lo mejor de cada disciplina y lo adaptamos a tus
+            necesidades y objetivos.
+          </p>
         </div>
       </div>
     </div>
